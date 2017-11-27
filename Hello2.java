4734ba0ec852fd3a87d077715d795e3cb505e3e8
SugarDoll <170148dm@yse-c.net>
2017-11-27 10:16:47 +0900

    Hello
    Test2

---
+++ Hello2.java
@@ -0,0 +1,21 @@
+import javax.swing.JFrame;
+
+public class Hello2 {
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame("Hello");
+		frame.setVisible(true);
+		
+
+	
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		
+		frame.setSize(960,540);
+		frame.setLocationRelativeTo(null);
+		
+	
+
+
+	}
+}
