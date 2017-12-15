b5673983d168b014cdae81fc72c78236e1742bd7
SugarDoll <33210364+SugarDoll@users.noreply.github.com>
2017-12-15 12:16:49 +0900

    Add files via upload

---
+++ Fukushu.java
@@ -0,0 +1,48 @@
+   import javax.swing.JFrame;
+   import javax.swing.JCheckBox;
+   import javax.swing.JLabel;
+   import javax.swing.JButton;
+   import javax.swing.JPanel;
+   import java.awt.BorderLayout;
+   import java.awt.GridLayout;
+   import java.awt.Container;
+   import java.awt.event.ActionEvent;
+   import java.awt.event.ActionListener;
+   
+   public class Fukushu extends JFrame implements ActionListener{
+   	private JLabel lbl;
+   	private JCheckBox check;
+    
+   	public static void main(String[] args){
+   		Fukushu fs = new Fukushu("復習！");
+   		fs.setVisible(true);
+   	}
+   	public void actionPerformed(ActionEvent e){
+   		if(this.check.isSelected()){
+   			this.lbl.setText(this.check.getText()+"が選択されています。");
+   		}else{
+   			this.lbl.setText("何も選択されてません。");
+   		}
+   	}
+   	Fukushu(String title){
+   		setTitle(title);
+   		setSize(300,100);
+   		setLocation(0,0);
+   		setDefaultCloseOperation(EXIT_ON_CLOSE);
+   		
+     		this.lbl = new JLabel("Hello");
+   		this.check = new JCheckBox("test");
+   		JPanel pnl1 = new JPanel();
+   		JPanel pnl2 = new JPanel();
+   		JButton btn = new JButton("Click!");
+   		btn.addActionListener(this);
+   
+   		pnl1.add(check);
+   		pnl1.add(this.lbl);
+   		pnl2.add(btn);
+   
+   		Container cp = getContentPane();
+   		cp.add(pnl1,BorderLayout.NORTH);
+   		cp.add(pnl2,BorderLayout.SOUTH);
+   	}
+   }
